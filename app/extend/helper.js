7d3ec8cc8ebc59760130f8fdcf2b85b344fcb82e
LittleSound <464388324@qq.com>
2021-03-09 13:15:49 +0800

    ✨ Added  "getMsgByThrowErr" helper

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -1,3 +1,19 @@
 // app/extend/helper.js
 const moment = require('moment')
-exports.relativeTime = time => moment(new Date(time * 1000)).fromNow()
+
+module.exports = {
+  relativeTime(time) { return moment(new Date(time * 1000)).fromNow() },
+
+  /**
+   * 根据捕获的错误返回错误信息 body， 如果不存在对应的错误信息 body 则继续弹出错误。
+   * @param {*} err 错误
+   * @param {*} msg ctx.msg 对象
+   */
+  getMsgByThrowErr(err, msg) {
+    if (typeof err === 'string') {
+      const body = msg[err]
+      if (body) return body
+    }
+    throw err
+  },
+}
